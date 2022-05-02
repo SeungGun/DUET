bfb6ead02c8cfb624dff6ed7616658826f4ac409
SeungGun <saverrw2864@naver.com>
2022-05-02 16:33:12 +0900

    comment[CreatePostActivity]: add function comment and inline comment

--- app/src/main/java/com/example/duet/board/CreatePostActivity.java
+++ app/src/main/java/com/example/duet/board/CreatePostActivity.java
@@ -4,7 +4,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -13,7 +12,6 @@ import androidx.exifinterface.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -24,6 +22,16 @@ import com.example.duet.R;
 
 import java.io.InputStream;
 
+/**
+ * 새로운 게시글을 추가할 수 있는 Activity
+ * @author Seungun Sin, 2022-05-02
+ */
+/*
+    To Do List
+    1. PostData 모델에 맞는 사용자 입력창 생성
+    2. 이미지 메타데이터와 입력한 정보를 Firestore 에 저장
+    3. 이미지를 Firebase Storage 에 저장
+ */
 public class CreatePostActivity extends AppCompatActivity {
     private LinearLayout imageContainer;
     private Button addImageButton;
@@ -39,6 +47,7 @@ public class CreatePostActivity extends AppCompatActivity {
         addImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // implicit intent 을 사용하여 갤러리 앱을 호출하여 이미지를 가져올 수 있도록함
                 Intent intent = new Intent();
                 intent.setType("image/* video/*"); // 파일 타입 지정 (이미지, 비디오)
                 intent.setAction(Intent.ACTION_GET_CONTENT);
@@ -51,39 +60,57 @@ public class CreatePostActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_CODE) { // 갤러리에서 사진을 가져오는 상황에 대한 처리
+            if (resultCode == RESULT_OK) { // 사진이 선택된 경우
                 try {
                     assert data != null;
-                    Uri selectedImageUri = data.getData();
+                    Uri selectedImageUri = data.getData(); // 선택한 사진에 대한 경로 값을 가져옴
 
                     InputStream inputStream = getContentResolver().openInputStream(selectedImageUri);
                     InputStream inputStream2 = getContentResolver().openInputStream(selectedImageUri);
+
+                    // 선택한 이미지를 Bitmap 형태로 생성
                     Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+
+                    // 선택한 이미지의 절대 경로를 통해 이미지의 메타데이터 값 추출
                     ExifInterface exifInterface = new ExifInterface(inputStream2);
                     int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+                    // 원본 bitmap 을 올바르게 회전한 bitmap 으로 변환
                     bitmap = rotateBitmap(bitmap, orientation);
+
                     inputStream.close();
                     inputStream2.close();
 
-                    ImageView imageView = new ImageView(getApplicationContext());
-                    imageView.setImageBitmap(bitmap);
+                    ImageView imageView = new ImageView(getApplicationContext()); // 동적 ImageView 생성
+                    imageView.setImageBitmap(bitmap); // ImageView 로 보여줄 이미지 설정
                     imageView.setScaleType(ImageView.ScaleType.FIT_XY); // 깔끔한 공간과 비율을 위해 필요
                     imageView.setAdjustViewBounds(true); // 깔끔한 공간과 비율을 위해 필요
 
-                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                    // 동적 레이아웃 속성 지정
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT
+                            , LinearLayout.LayoutParams.WRAP_CONTENT);
                     params.bottomMargin = 20;
+
+                    // LinearLayout 의 child 로 새로만든 ImageView 동적으로 추가
                     imageContainer.addView(imageView, params);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            else if(resultCode == RESULT_CANCELED){
+            else if(resultCode == RESULT_CANCELED){ // 사진 선택이 취소된 경우
                 Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
+    /**
+     * 불러온 이미지가 ImageView 에서 회전되는 현상이 있어서, Bitmap 자체에서 Exif 메타데이터를 이용해 제대로 회전시킨 이미지(Bitmap) 반환
+     * @author Seunggun Sin, 2022-05-02
+     * @param bitmap 원본 Bitmap 데이터
+     * @param orientation 원본 이미지의 현재 회전 상태 값(?)
+     * @return 제대로 회전시킨 뒤의 Bitmap 반환
+     */
     public Bitmap rotateBitmap(Bitmap bitmap, int orientation) {
         Matrix matrix = new Matrix();
         switch (orientation) {
