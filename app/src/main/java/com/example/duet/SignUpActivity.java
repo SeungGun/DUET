20927dc506a3be6699073a2c8378488a419ff739
SeungGun <saverrw2864@naver.com>
2022-05-06 17:36:58 +0900

    feat: when sign up, add default user profile image with url to Firebase

--- app/src/main/java/com/example/duet/SignUpActivity.java
+++ app/src/main/java/com/example/duet/SignUpActivity.java
@@ -4,6 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,6 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.duet.fragment.MainMenuActivity;
+import com.example.duet.util.FireStorage;
 import com.example.duet.util.Firestore;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,6 +25,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.UploadTask;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText inputEmail;
@@ -44,6 +52,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     /**
      * SignUpActivity 의 모든 View 들을 초기화 및 바인딩하는 작업
+     *
      * @author Seunggun Sin, 2022-05-01
      */
     private void bindingView() {
@@ -56,6 +65,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     /**
      * 사용자로부터 입력받은 4개의 필드로 새로운 사용자를 생성하고 DB에 저장하여 회원가입하도록 함
+     *
      * @author Seunggun Sin, 2022-05-01
      */
     private void doSignUp() {
@@ -70,26 +80,53 @@ public class SignUpActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             String userId = firebaseAuth.getCurrentUser().getUid(); // 생성한 유저의 uid 값 가져오기
 
+                            Drawable drawable = getResources().getDrawable(R.drawable.ic_launcher_foreground);
+                            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+                            Canvas canvas = new Canvas(bitmap);
+                            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                            drawable.draw(canvas);
+
+                            FireStorage.uploadUserProfileImage(bitmap
+                                    , userId)
+                                    .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                task.getResult().getStorage().getDownloadUrl()
+                                                        .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Uri> task) {
+                                                                if (task.isSuccessful()) {
+                                                                    Firestore.createNewUser(userId
+                                                                            , inputEmail.getText().toString()
+                                                                            , inputNickname.getText().toString()
+                                                                            , inputUserName.getText().toString(), task.getResult().toString())
+                                                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                                @Override
+                                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                                    // 데이터 생성 성공
+                                                                                    if (task.isSuccessful()) {
+                                                                                        Log.d("sign up success", "sucess");
+                                                                                        Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                                                                                        intent.putExtra("uid", userId);
+                                                                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                                                                        startActivity(intent);
+                                                                                    } else { // 데이터 생성 실패
+                                                                                        Log.d("sign up failure", "failure");
+                                                                                        firebaseAuth.signOut();
+                                                                                    }
+                                                                                }
+                                                                            });
+                                                                }
+                                                            }
+                                                        });
+                                            } else {
+                                                Log.e("upload profile image", "failure");
+                                            }
+                                        }
+                                    });
                             // Firestore 에 새로운 유저 데이터 저장
-                            Firestore.createNewUser(userId
-                                    , inputEmail.getText().toString()
-                                    , inputNickname.getText().toString()
-                                    , inputUserName.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    // 데이터 생성 성공
-                                    if (task.isSuccessful()) {
-                                        Log.d("sign up success", "sucess");
-                                        Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
-                                        intent.putExtra("uid", userId);
-                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                                        startActivity(intent);
-                                    } else { // 데이터 생성 실패
-                                        Log.d("sign up failure", "failure");
-                                        firebaseAuth.signOut();
-                                    }
-                                }
-                            });
+
                         } else {
                             // 유저 생성 실패
                         }
