f89026813b59443684163dc2e1f69d72e714c7ee
SeungGun <saverrw2864@naver.com>
2022-05-13 01:36:34 +0900

    add: multi view type to one recycler view for reply state

--- app/src/main/java/com/example/duet/adapter/TestReplyAdapter.java
+++ app/src/main/java/com/example/duet/adapter/TestReplyAdapter.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TestReplyAdapter extends RecyclerView.Adapter<TestReplyAdapter.ViewHolder> {
+public class TestReplyAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final ArrayList<ReplyData> replyDataArrayList;
     private final Context context;
 
@@ -25,19 +25,34 @@ public class TestReplyAdapter extends RecyclerView.Adapter<TestReplyAdapter.View
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
         Context context = parent.getContext();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.post_reply_item, parent, false);
-        TestReplyAdapter.ViewHolder viewHolder = new TestReplyAdapter.ViewHolder(view);
-        return viewHolder;
+        if (viewType == 0) {
+            view = inflater.inflate(R.layout.post_reply_item, parent, false);
+            return new NormalViewHolder(view);
+        } else if (viewType == 1) {
+            view = inflater.inflate(R.layout.reply_waiting_layout, parent, false);
+            return new TestReplyAdapter.WaitingViewHolder(view);
+        }
+        view = inflater.inflate(R.layout.post_reply_item, parent, false);
+        return new NormalViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull TestReplyAdapter.ViewHolder holder, int position) {
-        holder.writerNickname.setText(replyDataArrayList.get(position).getWriter().getNickname());
-        holder.body.setText((replyDataArrayList.get(position).getBody()));
-        holder.writeDate.setText(replyDataArrayList.get(position).getReplyDate().toString());
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof NormalViewHolder) {
+            ((NormalViewHolder) holder).writerNickname.setText(replyDataArrayList.get(position).getWriter().getNickname());
+            ((NormalViewHolder) holder).body.setText((replyDataArrayList.get(position).getBody()));
+            ((NormalViewHolder) holder).writeDate.setText(replyDataArrayList.get(position).getReplyDate().toString());
+        } else if (holder instanceof WaitingViewHolder) {
+            ((WaitingViewHolder) holder).writerNickname.setText(replyDataArrayList.get(position).getWriter().getNickname());
+            ((WaitingViewHolder) holder).writeDate.setText(replyDataArrayList.get(position).getReplyDate().toString());
+            ((WaitingViewHolder) holder).reliability.setText(replyDataArrayList.get(position).getWriter().getReliability()+"");
+        }
+
+
     }
 
     @Override
@@ -45,16 +60,34 @@ public class TestReplyAdapter extends RecyclerView.Adapter<TestReplyAdapter.View
         return replyDataArrayList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    @Override
+    public int getItemViewType(int position) {
+        return replyDataArrayList.get(position).getViewType();
+    }
+
+    public static class NormalViewHolder extends RecyclerView.ViewHolder {
         public TextView writerNickname;
         public TextView body;
         public TextView writeDate;
 
-        public ViewHolder(View itemView) {
+        public NormalViewHolder(View itemView) {
             super(itemView);
             writerNickname = itemView.findViewById(R.id.reply_item_writer);
             body = itemView.findViewById(R.id.reply_item_body);
             writeDate = itemView.findViewById(R.id.reply_item_date);
+        }
+    }
+
+    public static class WaitingViewHolder extends RecyclerView.ViewHolder {
+        public TextView writeDate;
+        public TextView writerNickname;
+        public TextView reliability;
+
+        public WaitingViewHolder(View itemView) {
+            super(itemView);
+            writeDate = itemView.findViewById(R.id.waiting_reply_date);
+            writerNickname = itemView.findViewById(R.id.waiting_reply_nickname);
+            reliability = itemView.findViewById(R.id.waiting_reply_reliability);
         }
     }
 }
