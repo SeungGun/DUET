0d4e00512b930ef230a4d5fda1a2fd7e12574673
SeungGun <saverrw2864@naver.com>
2022-05-14 17:10:16 +0900

    feat: function for changing profile image of user by selecting image from gallery or using default image

--- app/src/main/java/com/example/duet/fragment/MainMenuProfileFragment.java
+++ app/src/main/java/com/example/duet/fragment/MainMenuProfileFragment.java
@@ -1,18 +1,41 @@
 package com.example.duet.fragment;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.example.duet.R;
+import com.example.duet.board.TestUpdateProfile;
+import com.example.duet.model.User;
+import com.example.duet.util.FireStorage;
+import com.example.duet.util.Firestore;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.storage.UploadTask;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.CalendarMode;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
@@ -39,11 +62,27 @@ public class MainMenuProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private ImageView profileImage;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private Handler handler = new Handler(Looper.myLooper()){
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Firestore.getUserData(User.currentUser.getUid()).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()) {
+                        User.currentUser = task.getResult().toObject(User.class);
+                        Glide.with(getActivity())
+                                .load(User.currentUser.getProfileUrl())
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(profileImage);
+                    }
+                }
+            });
+        }
+    };
     public MainMenuProfileFragment() {
         // Required empty public constructor
     }
@@ -78,8 +117,63 @@ public class MainMenuProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.d("fragment", "onCreateView");
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_profile, container, false);
+        profileImage = rootView.findViewById(R.id.profile_photo);
+        profileImage.setScaleType(ImageView.ScaleType.FIT_XY); // 깔끔한 공간과 비율을 위해 필요
+        profileImage.setAdjustViewBounds(true); // 깔끔한 공간과 비율을 위해 필요
 
+        Glide.with(getActivity())
+                .load(User.currentUser.getProfileUrl())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(profileImage);
+        profileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("프로필 변경");
+                String[] items = {"갤러리에서 가져오기", "기본 이미지로 변경"};
+                builder.setItems(items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(which == 0){
+                            startActivity(new Intent(getActivity(), TestUpdateProfile.class));
+                        }
+                        else{
+                            FireStorage.uploadUserProfileImage(getDefaultProfileBitmap(), User.currentUser.getUid()).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                    if(task.isSuccessful()){
+                                        task.getResult().getStorage().getDownloadUrl()
+                                                .addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Uri> task) {
+                                                        if(task.isSuccessful()){
+                                                            Toast.makeText(getActivity(), "이미지 업데이트 성공", Toast.LENGTH_SHORT).show();
+                                                            Firestore.updateUserProfileUrl(User.currentUser.getUid()
+                                                                    , task.getResult().toString())
+                                                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                        @Override
+                                                                        public void onComplete(@NonNull Task<Void> task) {
+                                                                            if(task.isSuccessful()){
+                                                                                Message message = handler.obtainMessage();
+                                                                                handler.sendMessage(message);
+                                                                            }
+                                                                        }
+                                                                    });
+                                                        }
+                                                    }
+                                                });
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
         MaterialCalendarView materialCalendarView = rootView.findViewById(R.id.profile_calendar);
         materialCalendarView.setSelectedDate(CalendarDay.today());
 
@@ -101,5 +195,31 @@ public class MainMenuProfileFragment extends Fragment {
 
 
         return rootView;
+    }
+
+    public Bitmap getDefaultProfileBitmap(){
+        Drawable drawable = getResources().getDrawable(R.drawable.ic_profile_foreground);
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Firestore.getUserData(User.currentUser.getUid()).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()) {
+                    User.currentUser = task.getResult().toObject(User.class);
+                    Glide.with(getActivity())
+                            .load(User.currentUser.getProfileUrl())
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(profileImage);
+                }
+            }
+        });
     }
 }
