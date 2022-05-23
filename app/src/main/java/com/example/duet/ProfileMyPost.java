28fba5fdf1b3c6d48c06d2eaf6cf27b3f9781de7
juicyjung <travelandi01@gmail.com>
2022-05-23 14:56:20 +0900

    modify : app theme

--- app/src/main/java/com/example/duet/ProfileMyPost.java
+++ app/src/main/java/com/example/duet/ProfileMyPost.java
@@ -1,8 +1,17 @@
 package com.example.duet;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ProfileMyPost extends AppCompatActivity {
 
@@ -11,4 +20,8 @@ public class ProfileMyPost extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_my_post);
     }
+
+
+
+
 }
