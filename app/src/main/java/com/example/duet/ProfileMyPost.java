70dbebc6975ae2c4d8a9bddc1de4b7110b819ddd
juicyjung <travelandi01@gmail.com>
2022-05-23 14:34:30 +0900

    add ProfileMyPost

---
+++ app/src/main/java/com/example/duet/ProfileMyPost.java
@@ -0,0 +1,14 @@
+package com.example.duet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileMyPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_my_post);
+    }
+}
