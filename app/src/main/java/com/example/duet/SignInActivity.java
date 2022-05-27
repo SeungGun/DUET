7ef99b6af3034103135952938431dd4d6ee91f95
juicyjung <travelandi01@gmail.com>
2022-05-28 01:46:51 +0900

    add : putAutoLogin

--- app/src/main/java/com/example/duet/SignInActivity.java
+++ app/src/main/java/com/example/duet/SignInActivity.java
@@ -3,7 +3,9 @@ package com.example.duet;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -36,7 +38,8 @@ public class SignInActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doSignIn();
+                putAutoLogin(inputEmail.getText().toString(), inputPassword.getText().toString());
+                doSignIn(inputEmail.getText().toString(), inputPassword.getText().toString());
             }
         });
     }
@@ -55,11 +58,11 @@ public class SignInActivity extends AppCompatActivity {
      * 유저로부터 입력받은 이메일과 비밀번호로 로그인 처리 요청
      * @author Seunggun Sin, 2022-05-01
      */
-    private void doSignIn() {
+    protected void doSignIn(String id, String password) {
         /*
             입력 defensive 처리 필요
          */
-        firebaseAuth.signInWithEmailAndPassword(inputEmail.getText().toString(), inputPassword.getText().toString())
+        firebaseAuth.signInWithEmailAndPassword(id, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -79,6 +82,14 @@ public class SignInActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    protected void putAutoLogin(String id, String password){
+        SharedPreferences auto = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor autoLoginEdit = auto.edit();
+        autoLoginEdit.putString("id", id);
+        autoLoginEdit.putString("password", password);
+        autoLoginEdit.commit();
     }
 
 }
