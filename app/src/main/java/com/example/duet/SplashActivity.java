69225f7b7d5b6e25aa3c479dca900f599a31a05d
MinHyung Lee <ss49132002@gmail.com>
2022-06-02 22:48:56 +0900

    add: splash screen Add autologin in splash activity

---
+++ app/src/main/java/com/example/duet/SplashActivity.java
@@ -0,0 +1,77 @@
+package com.example.duet;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.duet.fragment.MainMenuActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashActivity extends AppCompatActivity {
+
+    String token;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        moveMain(2);
+
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                doAutoLogin();
+                finish();	//현재 액티비티 종료
+            }
+        }, 1000 * sec); // sec초 정도 딜레이를 준 후 시작
+    }
+
+    protected void doAutoLogin() {
+        SharedPreferences auto = getSharedPreferences("autoLogin", Activity.MODE_PRIVATE);
+        String id = auto.getString("id", null);
+        String password = auto.getString("password", null);
+
+        if (id != null && password != null) {
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+            firebaseAuth.signInWithEmailAndPassword(id, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String uid = firebaseAuth.getCurrentUser().getUid();
+                    /*
+                        FCM 사용할 때 토큰 가져오는 것도 필요할 것 같음
+                        firebaseAuth.getCurrentUser().getIdToken()
+                     */
+                                Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                                intent.putExtra("uid", uid);
+                                intent.putExtra("token", token);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(intent);
+                            } else {
+
+                            }
+                        }
+                    });
+        }
+        else {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+    }
+}
