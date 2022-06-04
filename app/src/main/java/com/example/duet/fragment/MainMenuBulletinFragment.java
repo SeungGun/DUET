506d33ad45a8276394e6428865b0a821a0fa3d69
MinHyung Lee <ss49132002@gmail.com>
2022-06-05 02:00:34 +0900

    add: swipe to refresh

--- app/src/main/java/com/example/duet/fragment/MainMenuBulletinFragment.java
+++ app/src/main/java/com/example/duet/fragment/MainMenuBulletinFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -49,6 +50,7 @@ public class MainMenuBulletinFragment extends Fragment {
     private Chip categoryProgramming;
     private Chip categoryArt;
     private Chip categoryKorean;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private ArrayList<PostData> selectArrayList;
     private boolean isInitial = true;
     private ArrayList<String> currentSelectedCategoryList;
@@ -65,6 +67,21 @@ public class MainMenuBulletinFragment extends Fragment {
         currentSelectedCategoryList = new ArrayList<>();
         postType = rootView.findViewById(R.id.sefChip);
         categoryMath = rootView.findViewById(R.id.mathChip);
+        swipeRefreshLayout = rootView.findViewById(R.id.pullToRefresh);
+
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //TODO 새로운 게시글 가져오기
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+
+        });
+
+
+
         categoryMath.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -78,6 +95,7 @@ public class MainMenuBulletinFragment extends Fragment {
             }
         });
         categoryScience = rootView.findViewById(R.id.scienceChip);
+
         categoryScience.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
